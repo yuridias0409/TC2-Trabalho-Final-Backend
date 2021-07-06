afb01fa6e7aaf3bb16cd37f4d3be8a53594b6b85
pedrochr <pedrochserra@gmail.com>
2021-07-06 19:34:44 -0300

    Testando a mudança pra arrumar o cors.

--- config/express.js
+++ config/express.js
@@ -4,8 +4,9 @@ const config = require('config');
 
 module.exports = () => {
   const app = express();
-  app.use(bodyParser.urlencoded({ extended: false }))
-  app.use(bodyParser.json())
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(bodyParser.json());
+  const cors = require('cors');
 
   // SETANDO VARIÁVEIS DA APLICAÇÃO
   app.set('port', process.env.PORT);
@@ -13,14 +14,15 @@ module.exports = () => {
   // MIDDLEWARES
   app.use((req, res, next) => {
     //Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "" indicando que qualquer site pode fazer a conexão
-    res.header("Access-Control-Allow-Origin", "");
+    res.header("Access-Control-Allow-Origin", "*");
     req.header("Access-Control-Allow-Origin", "*");
     
     //Quais são os métodos que a conexão pode realizar na API
     res.header("Access-Control-Allow-Methods", 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
     req.header("Access-Control-Allow-Methods", 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
     res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
-
+    app.use(cors());
+    
     // Parseia também requisições do tipo HTML - application/x-www-form-urlencoded
     next();
   });
